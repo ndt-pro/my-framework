bed87949f36cc9a898b632e46c77c303ebaf84ac
ndt-pro <toannguyen080199@gmail.com>
2020-12-03 15:01:22 +0700

    update ndt cli

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,26 +9,7 @@ use App\Model\Users;
 class HomeController extends Controller {
 
     public function index() {
-        // $id = DB::table("users")->insert([
-        //     'username' => "test",
-        //     'password' => "abcxyz"
-        // ]);
-
-        // $users = DB::table("users")->where(['username' => 'test'])->take(3)->get();
-
-
-        // $result = DB::table('users')->where(['id' => 6])->delete();
-
-        // $user = new Users();
-
-        // $user->username = "new user";
-        // $user->password = "new pass";
-
-        // $result = $user->save();
-
-        $users = Users::get();
-
-        return response()->json($users);
+        return response()->code(200);
     }
 
     public function getApiNoAuth() {
