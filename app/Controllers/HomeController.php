a0eed5c12b55a238d78f677f221132ccd2df7510
ndt-pro <toannguyen080199@gmail.com>
2020-12-08 16:05:56 +0700

    toan 1 commit 2

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller {
 
     public function index() {
         return response()->json([
-            'welcome' => 'Xin chào các bạn'
+            'welcome' => 'toàn 1 Xin chào các bạn'
         ]);
     }
 
